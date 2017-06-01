0d57d218d057df233d84b24908fd3ed2ca1eae41
taineleau <taineleau@gmail.com>
2017-06-01 17:05:44 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=1
+#export MXNET_ENGINE_TYPE=NaiveEngine
+#export MXNET_BACKWARD_DO_MIRROR=1
 
-## train densenet-169 
-python -u train_densenet.py --data-dir data/imagenet --data-type imagenet --depth 169 --batch-size 256 --growth-rate 32 --drop-out 0 --reduction 0.5 --gpus=6,7,8,9
+python2 -u train_densenet.py --data-dir ./data/cifar10 --data-type cifar10 --depth 100 --batch-size 64 --growth-rate 12 --drop-out 0 --reduction 0.5 --gpus=0 --aug-level  1
